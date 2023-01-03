efc33e1243b0b66ee23152b5d94d3a2d9bd3cda7
danilsyah <danilsyaharihardjo@gmail.com>
2023-01-03 16:19:53 +0700

    update includes backsite

--- resources/views/includes/frontsite/meta.blade.php
+++ resources/views/includes/frontsite/meta.blade.php
@@ -1,3 +1,11 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+{{-- seo setting base --}}
+<meta name="description" content="{{ 'Meet Your Doctor. Trusted & Professional' }}">
+<meta name="keywords" content="{{ 'doctor, meetdoctor, temudokter, appointment, trusted & professional' }}">
+<meta name="author" content="{{ 'Temu Dokter' }}">
+
+{{-- CSRF Token --}}
+<meta name="csrf-token" content="{{ csrf_token() }}">
